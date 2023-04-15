e23b934489ef2b669c1634b1efd329216df6df08
MBilik <dexeraz@gmail.com>
2023-04-15 21:17:03 +0200

    Colors appear only after attempt is submited

--- src/components/Letter.tsx
+++ src/components/Letter.tsx
@@ -7,7 +7,7 @@ type Props = {
 };
 
 const Letter = (props: Props) => {
-  const { board, correctWord } = useContext(AppContext);
+  const { board, correctWord, currAttempt } = useContext(AppContext);
   const letter = board[props.attemptVal][props.letterPos];
 
   const correct = correctWord[props.letterPos] === letter;
@@ -16,10 +16,12 @@ const Letter = (props: Props) => {
   const almost = !correct && letter !== "" && correctWord.includes(letter);
 
   //if it is correct, then correct if not then is it almost, if not the we set error
-  const letterState = correct ? "correct" : almost ? "almost" : "error";
+  const letterState =
+    currAttempt.attempt > props.attemptVal &&
+    (correct ? "correct" : almost ? "almost" : "error");
 
   return (
-    <div className="letter" id={letterState}>
+    <div className="letter" id={letterState.toString()}>
       {letter}
     </div>
   );
