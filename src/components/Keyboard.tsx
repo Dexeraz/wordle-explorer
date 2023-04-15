a95219896bb9c42610f4745236c81de0cc223561
MBilik <dexeraz@gmail.com>
2023-04-15 20:50:16 +0200

    Detecting the keyboard typing

--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -1,30 +1,85 @@
-import React from "react";
+import React, { useCallback, useEffect, useContext } from "react";
+import { AppContext } from "../App";
 import Key from "./Key";
 
 type Props = {};
 
 const Keyboard = (props: Props) => {
+  const { currAttempt, onSelectLetter, onEnter, onDelete } =
+    useContext(AppContext);
   const keys1 = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
   const keys2 = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
   const keys3 = ["Z", "X", "C", "V", "B", "N", "M"];
+
+  const handleKeyboard = useCallback(
+    (event: any) => {
+      if (event.key === "Enter") {
+        onEnter();
+      } else if (event.key === "Backspace") {
+        onDelete();
+      } else {
+        // looping though arrey to check if pressed key is the key in the array and we pass it to onSelectLetter
+        keys1.forEach((key) => {
+          if (event.key.toLowerCase() === key.toLowerCase()) {
+            onSelectLetter(key);
+          }
+        });
+        keys2.forEach((key) => {
+          if (event.key.toLowerCase() === key.toLowerCase()) {
+            onSelectLetter(key);
+          }
+        });
+        keys3.forEach((key) => {
+          if (event.key.toLowerCase() === key.toLowerCase()) {
+            onSelectLetter(key);
+          }
+        });
+      }
+    },
+    [currAttempt]
+  );
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyboard);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyboard);
+    };
+  }, [handleKeyboard]);
+
   return (
-    <div className="keyboard">
+    <div className="keyboard" onKeyDown={handleKeyboard}>
       <div className="line1">
         {keys1.map((key) => {
-            return <div> <Key keyVal={key} bigKey={false}/> </div>
+          return (
+            <div>
+              {" "}
+              <Key keyVal={key} bigKey={false} />{" "}
+            </div>
+          );
         })}
       </div>
       <div className="line2">
-      {keys2.map((key) => {
-            return <div> <Key keyVal={key} bigKey={false}/> </div>
+        {keys2.map((key) => {
+          return (
+            <div>
+              {" "}
+              <Key keyVal={key} bigKey={false} />{" "}
+            </div>
+          );
         })}
       </div>
       <div className="line3">
-        <Key keyVal={"ENTER"} bigKey={true}/>
-      {keys3.map((key) => {
-            return <div> <Key keyVal={key} bigKey={false}/> </div>
+        <Key keyVal={"ENTER"} bigKey={true} />
+        {keys3.map((key) => {
+          return (
+            <div>
+              {" "}
+              <Key keyVal={key} bigKey={false} />{" "}
+            </div>
+          );
         })}
-        <Key keyVal={"DELETE"} bigKey={true}/>
+        <Key keyVal={"DELETE"} bigKey={true} />
       </div>
     </div>
   );
