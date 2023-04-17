d57fb6e048c8316a3d67be29bc66b4e1a73f9e00
MBilik <dexeraz@gmail.com>
2023-04-17 10:00:22 +0200

    Changed word bank to json file

--- src/helpers/Words.ts
+++ src/helpers/Words.ts
@@ -1,4 +1,4 @@
-import wordBank from "../wordle-bank.txt";
+import wordBank from "../words.json";
 
 export const boardDefault = [
   ["", "", "", "", ""],
@@ -14,16 +14,10 @@ type WordsType = {
   todaysWord: string;
 };
 
+export const generateWordSet = async (): Promise<WordsType> => {
+  const wordArr: string[] = wordBank;
+  const todaysWord = wordArr[Math.floor(Math.random() * wordArr.length)];
+  const wordSet = new Set(wordArr);
 
-export const generateWordSet = async () => {
-  let wordSet;
-  let todaysWord;
-  await fetch(wordBank)
-    .then((response) => response.text())
-    .then((result) => {
-      const wordArr = result.split("\r\n"); //split changes the string to a array based on a given parameter
-      todaysWord = wordArr[Math.floor(Math.random() * wordArr.length)]; //get random world per run
-      wordSet = new Set(wordArr); //we can use Set because we have unique values
-    });
   return { wordSet, todaysWord };
 };
